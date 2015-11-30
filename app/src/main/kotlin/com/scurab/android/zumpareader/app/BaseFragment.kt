d93955a3e52ea101373f6ffed81ea24618afb92f
Jiri Bruchanov <jbruchanov@gmail.com>
2015-11-30 20:44:21 +0000

    Update BaseFragment add abstract isLoading stuff

--- app/src/main/kotlin/com/scurab/android/zumpareader/app/BaseFragment.kt
+++ app/src/main/kotlin/com/scurab/android/zumpareader/app/BaseFragment.kt
@@ -22,14 +22,6 @@ open public abstract class BaseFragment : Fragment() {
             return mainActivity?.zumpaApp
         }
 
-    public open fun onMenuItemClick(item: MenuItem): Boolean {
-        return false
-    }
-
-    public open fun openFragment(fragment: BaseFragment, addToBackStack: Boolean = true, replace: Boolean = true) {
-        mainActivity?.openFragment(fragment, addToBackStack, replace)
-    }
-
     public var progressBarVisible: Boolean
         get() {
             return mainActivity?.progressBarVisible ?: false
@@ -38,15 +30,32 @@ open public abstract class BaseFragment : Fragment() {
             mainActivity?.progressBarVisible = value
         }
 
+    private var _isLoading: Boolean = false
+    protected open var isLoading: Boolean
+        get() {
+            return _isLoading
+        }
+        set(value) {
+            progressBarVisible = value
+            _isLoading = value
+        }
+
     private var _zumpaData: TreeMap<String, ZumpaThread>? = null
     protected val zumpaData: TreeMap<String, ZumpaThread> by lazy { _zumpaData!! }
+    protected abstract val title: CharSequence
+
+    public open fun onMenuItemClick(item: MenuItem): Boolean {
+        return false
+    }
+
+    public open fun openFragment(fragment: BaseFragment, addToBackStack: Boolean = true, replace: Boolean = true) {
+        mainActivity?.openFragment(fragment, addToBackStack, replace)
+    }
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         _zumpaData = zumpaApp!!.zumpaData
     }
-
-    protected abstract val title: CharSequence
 
     override fun onResume() {
         super.onResume()
