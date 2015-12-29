a41cce4ed762bd4ae6e1d5ea65a33863b7650b28
Jiri Bruchanov <jbruchanov@gmail.com>
2015-12-29 20:35:15 +0000

    Add simple button preference

---
+++ app/src/main/kotlin/com/scurab/android/zumpareader/preferences/ButtonPreference.kt
@@ -0,0 +1,15 @@
+package com.scurab.android.zumpareader.preferences
+
+import android.content.Context
+import android.preference.DialogPreference
+import android.util.AttributeSet
+
+/**
+ * Created by JBruchanov on 29/12/2015.
+ */
+public class ButtonPreference(context: Context?, attrs: AttributeSet?) : DialogPreference(context, attrs) {
+
+    override fun onClick() {
+
+    }
+}
