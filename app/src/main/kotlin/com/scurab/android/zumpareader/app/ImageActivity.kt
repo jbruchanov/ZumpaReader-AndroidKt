2396d9a76054c18e550ed91e5241b0353dd2c310
Jiri Bruchanov <jbruchanov@gmail.com>
2017-09-10 13:05:29 +0100

    Fix incorrect Fresco usages

--- app/src/main/kotlin/com/scurab/android/zumpareader/app/ImageActivity.kt
+++ app/src/main/kotlin/com/scurab/android/zumpareader/app/ImageActivity.kt
@@ -2,21 +2,19 @@ package com.scurab.android.zumpareader.app
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.webkit.WebView
 import android.widget.ImageView
 import com.bogdwellers.pinchtozoom.ImageMatrixTouchHandler
 import com.facebook.common.executors.CallerThreadExecutor
 import com.facebook.common.references.CloseableReference
+import com.facebook.datasource.BaseDataSubscriber
 import com.facebook.datasource.DataSource
+import com.facebook.datasource.DataSources
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber
+import com.facebook.imagepipeline.image.CloseableBitmap
 import com.facebook.imagepipeline.image.CloseableImage
-import com.facebook.imagepipeline.request.ImageRequestBuilder
+import com.facebook.imagepipeline.request.ImageRequest
 import com.scurab.android.zumpareader.R
 import com.scurab.android.zumpareader.util.startLinkActivity
 import org.jetbrains.anko.imageBitmap
@@ -43,33 +41,42 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_image)
-        imageView = findViewById<ImageView>(R.id.image)
+        imageView = findViewById(R.id.image)
         imageView.setOnTouchListener(ImageMatrixTouchHandler(this))
         loadImage(url)
     }
 
     private fun loadImage(url: String) {
-        val imageRequest = ImageRequestBuilder
-                .newBuilderWithSource(Uri.parse(url))
-                .build()
-
+        val imageRequest = ImageRequest.fromUri(url)
         val imagePipeline = Fresco.getImagePipeline()
-        val dataSource = imagePipeline.fetchDecodedImage(imageRequest, this)
-        dataSource.subscribe(object : BaseBitmapDataSubscriber() {
-            override fun onNewResultImpl(bitmap: Bitmap?) {
-                if (dataSource.isFinished && bitmap != null) {
-                    imageView.post {
-                        imageView.imageBitmap = bitmap
+        var dataSource = imagePipeline.fetchDecodedImage(imageRequest, this)
+        try {
+            val result = DataSources.waitForFinalResult(dataSource)
+            if (result != null) {
+                dataSource.subscribe(object : BaseDataSubscriber<CloseableReference<CloseableImage>>() {
+                    override fun onNewResultImpl(dataSource: DataSource<CloseableReference<CloseableImage>>) {
+                        if (!dataSource.isFinished) {
+                            return
+                        }
+                        val closeableImageRef = dataSource.result
+                        (closeableImageRef?.get() as? CloseableBitmap)?.let {
+                            imageView.post {
+                                imageView.imageBitmap = it.underlyingBitmap
+                            }
+                        }
+                        dataSource.close()
                     }
-                }
-            }
 
-            override fun onFailureImpl(dataSource: DataSource<CloseableReference<CloseableImage>>?) {
-                startLinkActivity(url)
-                finish()
-                dataSource?.close()
-            }
+                    override fun onFailureImpl(dataSource: DataSource<CloseableReference<CloseableImage>>?) {
+                        startLinkActivity(url)
+                        finish()
+                        dataSource?.close()
+                    }
 
-        }, CallerThreadExecutor.getInstance())
+                }, CallerThreadExecutor.getInstance())
+            }
+        } finally {
+            dataSource?.close()
+        }
     }
 }
