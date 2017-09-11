4f9582c1240a7316981635c0edc44f2429bf295f
Jiri Bruchanov <jbruchanov@gmail.com>
2017-09-11 21:55:19 +0100

    Fix loading images in ImageActivity

--- app/src/main/kotlin/com/scurab/android/zumpareader/app/ImageActivity.kt
+++ app/src/main/kotlin/com/scurab/android/zumpareader/app/ImageActivity.kt
@@ -2,19 +2,21 @@ package com.scurab.android.zumpareader.app
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import com.bogdwellers.pinchtozoom.ImageMatrixTouchHandler
 import com.facebook.common.executors.CallerThreadExecutor
+import com.facebook.common.memory.PooledByteBuffer
 import com.facebook.common.references.CloseableReference
 import com.facebook.datasource.BaseDataSubscriber
 import com.facebook.datasource.DataSource
 import com.facebook.datasource.DataSources
+import com.facebook.datasource.DataSubscriber
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.imagepipeline.image.CloseableBitmap
 import com.facebook.imagepipeline.image.CloseableImage
-import com.facebook.imagepipeline.request.ImageRequest
+import com.facebook.imagepipeline.image.CloseableStaticBitmap
 import com.scurab.android.zumpareader.R
 import com.scurab.android.zumpareader.util.scaledImageRequest
 import com.scurab.android.zumpareader.util.startLinkActivity
@@ -44,39 +46,76 @@ class ImageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image)
         imageView = findViewById(R.id.image)
         imageView.setOnTouchListener(ImageMatrixTouchHandler(this))
-        loadImage(url)
+        loadImageFromCache(url)
+    }
+
+    private fun loadImageFromCache(url: String) {
+        val imagePipeline = Fresco.getImagePipeline()
+        //TODO: is there better way how to get a bitmap with not crashing in case of invalid image ?
+        //ImageMatrixTouchHandler doesn't work with fresco imageview
+        var dataSource = imagePipeline.fetchImageFromBitmapCache(scaledImageRequest(url, this), this)
+        dataSource.subscribe(object : BaseDataSubscriber<CloseableReference<CloseableImage>>() {
+            override fun onNewResultImpl(dataSource: DataSource<CloseableReference<CloseableImage>>) {
+                if (dataSource.isFinished) {
+                    val bitmap = (dataSource.result?.get() as? CloseableStaticBitmap)?.underlyingBitmap
+                    if (bitmap != null) {
+                        imageView.imageBitmap = bitmap
+                    } else {
+                        loadImage(url)
+                    }
+                }
+            }
+
+            override fun onFailureImpl(dataSource: DataSource<CloseableReference<CloseableImage>>?) {
+                loadImage(url)
+            }
+
+        }, CallerThreadExecutor.getInstance())
     }
 
     private fun loadImage(url: String) {
         val imagePipeline = Fresco.getImagePipeline()
-        var dataSource = imagePipeline.fetchDecodedImage(scaledImageRequest(url, this), this)
+        var dataSource = imagePipeline.fetchEncodedImage(scaledImageRequest(this.url, this), this)
         try {
             val result = DataSources.waitForFinalResult(dataSource)
             if (result != null) {
-                dataSource.subscribe(object : BaseDataSubscriber<CloseableReference<CloseableImage>>() {
-                    override fun onNewResultImpl(dataSource: DataSource<CloseableReference<CloseableImage>>) {
-                        if (!dataSource.isFinished) {
-                            return
-                        }
-                        val closeableImageRef = dataSource.result
-                        (closeableImageRef?.get() as? CloseableBitmap)?.let {
-                            imageView.post {
-                                imageView.imageBitmap = it.underlyingBitmap
+                dataSource.subscribe(object : DataSubscriber<CloseableReference<PooledByteBuffer>> {
+                    override fun onNewResult(dataSource: DataSource<CloseableReference<PooledByteBuffer>>) {
+                        if (dataSource.isFinished) {
+                            try {
+                                val buffer = dataSource.result!!.get()
+                                val imgData = ByteArray(buffer.size())
+                                buffer.read(0, imgData, 0, imgData.size)
+                                val bitmap = BitmapFactory.decodeByteArray(imgData, 0, imgData.size)
+                                if (bitmap == null) {
+                                    onOpenLinkOnError(url)
+                                } else {
+                                    imageView.imageBitmap = bitmap
+                                }
+                            } catch (t: Throwable) {
+                                onOpenLinkOnError(url)
+                            } finally {
+                                dataSource.close()
                             }
                         }
-                        dataSource.close()
                     }
 
-                    override fun onFailureImpl(dataSource: DataSource<CloseableReference<CloseableImage>>?) {
-                        startLinkActivity(url)
-                        finish()
+                    override fun onFailure(dataSource: DataSource<CloseableReference<PooledByteBuffer>>?) {
+                        onOpenLinkOnError(url)
                         dataSource?.close()
                     }
 
+                    override fun onCancellation(dataSource: DataSource<CloseableReference<PooledByteBuffer>>?) {}
+                    override fun onProgressUpdate(dataSource: DataSource<CloseableReference<PooledByteBuffer>>?) {}
                 }, CallerThreadExecutor.getInstance())
             }
         } finally {
             dataSource?.close()
         }
+    }
+
+    private fun onOpenLinkOnError(url: String) {
+        startLinkActivity(url)
+        finish()
     }
 }
