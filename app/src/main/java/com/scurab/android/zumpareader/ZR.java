c0b45e002b2efa36b7ac4b9b3fdb3d0adab15c31
Jiri Bruchanov <jbruchanov@gmail.com>
2019-08-27 21:40:58 +0100

    Update to zunpa.cz

--- app/src/main/java/com/scurab/android/zumpareader/ZR.java
+++ app/src/main/java/com/scurab/android/zumpareader/ZR.java
@@ -2,7 +2,7 @@ package com.scurab.android.zumpareader;
 
 public final class ZR {
     public static final class Constants {
-        public static final String ZUMPA_MAIN_URL = "http://portal2.dkm.cz";
+        public static final String ZUMPA_MAIN_URL = "https://zunpa.cz";
         public static final String ZUMPA_WS_MAIN_URL = "http://zumpaws.scurab.com:8104";
         public static final String ZUMPA_PHP_MAIN_URL = "http://zumpareader.scurab.com";
         public static final String ENCODING = "ISO-8859-2";
