b3b7d5ac411e1b24db7e120fad37392591304975
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-22 20:27:33 +0100

    Clean up

--- app/src/main/kotlin/com/scurab/android/zumpareader/app/MainActivity.kt
+++ app/src/main/kotlin/com/scurab/android/zumpareader/app/MainActivity.kt
@@ -63,7 +63,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Fabric.with(this, Crashlytics())
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
